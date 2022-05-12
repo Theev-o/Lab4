f18bcfdd951ae968eeb952b70c5575efd14c46c5
Theev-o <dsw46651@student.dsw.edu.pl>
2022-05-12 14:39:16 +0200

    pierwszy myslnik

--- Untitled-1.sh
+++ Untitled-1.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+VALID_ARGS=$(getopt -o d --long date -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+eval set -- "$VALID_ARGS"
+while [ : ]
+do
+    case "${1}" in
+        -d | --date) 
+        date +%m-%d-%Y
+        break
+    esac
+done
