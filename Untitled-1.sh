126b5f971c7b6c440891ef0367cf5d82abc93308
Theev-o <dsw46651@student.dsw.edu.pl>
2022-05-12 14:57:12 +0200

    logs

--- Untitled-1.sh
+++ Untitled-1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VALID_ARGS=$(getopt -o d --long date -- "$@")
+VALID_ARGS=$(getopt -o dl --long date,logs -- "$@")
 if [[ $? -ne 0 ]]; then
     exit 1;
 fi
@@ -10,7 +10,15 @@ while [ : ]
 do
     case "${1}" in
         -d | --date) 
-        date +%m-%d-%Y
-        break
+            date +%m-%d-%Y
+            break
+            ;;
+        -l | --logs)
+            for i in {1..100}
+                do
+                    echo "log$i.txt, $(basename "$0"), $(date +%m-%d-%Y)" >> log$i.txt
+              done
+              break
+              ;;
     esac
 done
