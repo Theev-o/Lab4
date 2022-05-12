4fb0d35f038a8e8df0fc029a4d3954d7344cd0b6
Theev-o <dsw46651@student.dsw.edu.pl>
2022-05-12 16:12:48 +0200

    Finished

--- Untitled-1.sh
+++ Untitled-1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VALID_ARGS=$(getopt -o dll:h --long date,logs,logs:,help -- "$@")
+VALID_ARGS=$(getopt -o dll:hee: --long date,logs,logs:,help,init,error,error: -- "$@")
 if [[ $? -ne 0 ]]; then
     exit 1;
 fi
@@ -21,13 +21,30 @@ do
                 fi
             for i in $(seq 1 $COUNT)
                 do
-                    echo "log$i.txt, $(basename "$0"), $(date +%m-%d-%Y)" >> log$i.txt
+                    echo "log$i.txt, $(basename "$0"), $(date +%m-%d-%Y)" >> ./log$i/log$i.txt
               done
               break
               ;;
         -h | --help)
-                echo "Usage:--date displays current date. --logs creates specified number of log files, default is 100. --help displays this message"
-                break
-                ;;
+            echo "Usage:-d or --date displays current date. -l or --logs creates specified number of log files, default is 100, -e or --error creates specified amount of error files, default is 100. --init clones the repository and adds this script to system PATH. -h or --help displays this message."
+            break
+            ;;
+        --init)
+        git clone https://github.com/Theev-o/Lab4.git ./
+        export PATH=$PATH:$(dirname -- "$(readlink -f "${BASH_SOURCE}")")
+        break
+        ;;
+        -e | --error)
+            if [ $# -eq 3 ]; then
+                    COUNT=$3;
+                else
+                    COUNT=100;
+                fi
+            for i in $(seq 1 $COUNT)
+                do
+                    echo "error$i.txt, $(basename "$0"), $(date +%m-%d-%Y)" >> ./error$i/error$i.txt
+              done
+              break
+              ;;
     esac
 done
